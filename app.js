c31fbab2cb192d3ac3477e7d693b0791bd6e16ae
Nharvey8 <70868010+Nharvey8@users.noreply.github.com>
2024-11-04 14:17:07 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html'); // send HTML file on GET request
+    res.sendFile(__dirname + '/contact.html'); // send HTML file on GET request
 });
 
 app.post('/submit-form', (req, res) => {
     const username = req.body.username; // access form data
     // Add validation logic here
-    res.send(`Username is $nharve14`);
+    res.send(`Thank you`);
 });
 
 app.listen(port, () => {
-    console.log(`Server running on http://localhost:${port}`);
+    console.log(`Server running on http://nharvey8.github.io:${port}`);
 });
